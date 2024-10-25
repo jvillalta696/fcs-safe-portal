6ab0421d0372cafc14900ea791e3234cf206f161
jvillalta696 <jvillalta696@gmail.com>
2024-10-24 19:29:26 -0600

    start project

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import NavBar from './componets/NavBar.jsx';
+
+
+function App() {
+
+
+  return (
+    <>
+     <NavBar/>
+    </>
+  )
+}
+
+export default App
